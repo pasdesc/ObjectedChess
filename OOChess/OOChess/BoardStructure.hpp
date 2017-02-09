4389ecab69940d9d2e10849592dfed2a650e66d4
pasdesc <pasdesc@nuevaschool.org>
2017-02-09 07:54:43 -0800

    Addded the Board class to BoardStructure.hpp

--- OOChess/OOChess/BoardStructure.hpp
+++ OOChess/OOChess/BoardStructure.hpp
@@ -9,14 +9,25 @@
 #ifndef BoardStructure_hpp
 #define BoardStructure_hpp
 #include <stdio.h>
+class piece;
 class Board{
     public:
-        Board();
-        ~Board();
-        Board(const Board &copy);
-        Board operator=(const Board &copy);
-        bool inline getTurn();
-        void movePiece(short int x1, short int y1, short int x2, short int y2);
-        void inline switchTurn();
+        Board(); //Constructor
+        ~Board(); //Destructor
+        Board(const Board &copy); // Copy Constructor
+        Board operator=(const Board &copy); //Assignment operator
+        bool inline getTurn(); //Check whose turn it is
+        void movePiece(short int x1, short int y1, short int x2, short int y2); //Move a piece from (x1,y1) to (x2,y2)
+        void inline switchTurn(); //Switch the turn
+        void capturePiece(int x,int y); //Move a piece to the captured zone off of the board
+        int score(const Board &scored); //Score a board
+    private:
+        piece** mainBoard[8][8]; //Pointer array representing the chess board
+        bool turn; //Whose turn it is
+        piece* whiteCaptured[16]; //White's captured zone
+        piece* blackCaptured[16]; //Black's captured zone
+    
+    
+    
 };
 #endif /* BoardStructure_hpp */
--- OOChess/OOChess/main.cpp
+++ OOChess/OOChess/main.cpp
@@ -10,7 +10,6 @@
 #include "BoardStructure.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
